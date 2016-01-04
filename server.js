6557b8ffd483aa41961038573efe1d366d82a456
Lissa Walzer <lissawalzer@gmail.com>
2016-01-04 13:25:46 -0800

    Added the server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('Express');
+var pg = require('pg');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var locus = require('locus');
+var methodOverride = require('method-override');
+var app = express();
+
+// middleware
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+	extended: true}
+	));
+app.use(morgan('tiny'));
+app.use(methodOverride('_method'));
+
+var server = app.listen(process.env.PORT || 3000, function () {
+var port = server.address().port;
+  console.log('Server up and listening on', port);
+});
